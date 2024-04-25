4768e7199ef3208fb6b5603fcf73ccf2d74f7318
Smit Dagli <smitdagli@Smits-Laptop.local>
2024-04-25 13:19:51 -0700

    Initial commit, backend, env

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
 from flask import Flask, request, jsonify, send_from_directory
 from openai import OpenAI
 from flask_cors import CORS
-from dotenv import load_dotenv
-import os
 
 app = Flask(__name__, static_folder='frontend/disaster-prep-frontend/build', static_url_path='')
 CORS(app)
 
-# Set up the OpenAI API key directly
-load_dotenv()
-openai_api_key = os.environ.get("OPENAI_API_KEY")
-client = OpenAI(api_key=openai_api_key)
+client = OpenAI(api_key="replace_with_actual_key")
 
 # In-memory storage for user profiles
 user_profiles = {}
